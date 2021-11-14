77e04677746ffb44da14318c18f9fce3c36fa9d1
Dmitry Luzin <d.luzin@ongrid.pro/>
2021-11-14 16:31:01 +0300

    les3 add save function on userprofile

--- frontend/main.js
+++ frontend/main.js
@@ -35,10 +35,41 @@ logout = async () => {
 openUserInfo = async () => {
     user = await Moralis.User.current();
     if (user){    
+        const email = user.get('email');
+        if(email){
+            userEmailField.value = email;
+        }else{
+            userEmailField.value = "";
+        }
+
+        userUsernameField.value = user.get('username');
+
+        const userAvatar = user.get('avatar');
+        if(userAvatar){
+            userAvatarImg.src = userAvatar.url();
+            showElement(userAvatarImg);
+        }else{
+            hideElement(userAvatarImg);
+        }
+
         showElement(userInfo);
     }else{
         login();
     }
+}
+
+saveUserInfo = async () => {
+    user.set('email', userEmailField.value);
+    user.set('username', userUsernameField.value);
+
+    if (userAvatarFile.files.length > 0) {
+        const avatar = new Moralis.File("avatar1.jpg", userAvatarFile.files[0]);
+        user.set('avatar', avatar);
+    }
+
+    await user.save();
+    alert("User info saved successfully!");
+    openUserInfo();
 }
 
 hideElement = (element) => element.style.display = "none";
@@ -54,8 +85,15 @@ userProfileButton.onclick = openUserInfo;
 // userProfileButton.onclick = openUserInfo;
 
 const userinfo  = document.getElementById("userinfo");
+const userUsernameField = document.getElementById("txtUsername");
+const userEmailField = document.getElementById("txtEmail");
+const userAvatarImg = document.getElementById("imgAvatar");
+const userAvatarFile = document.getElementById("fileAvatar");
+
 document.getElementById("btnCloseUserInfo").onclick = () => hideElement(userInfo);
 document.getElementById("btnLogout").onclick = logout;
+document.getElementById("btnSaveUserInfo").onclick = saveUserInfo;
+
 
 
 

26bcaad5e25b20385071e35eebca41b05875c500
Dmitry Luzin <d.luzin@ongrid.pro/>
2021-11-14 15:46:33 +0300

    les2 add simple user profile

--- frontend/main.js
+++ frontend/main.js
@@ -2,6 +2,7 @@ Moralis.initialize("q7Gl3yXVfabVqt2iEO9t9eX7En4U7jccpRyjYAVq");
 Moralis.serverURL = 'https://4u6qe0qwyedr.usemoralis.com:2053/server'
 
 init = async () => {
+    hideElement(userinfo);
     window.web3 = await Moralis.Web3.enable();
     initUser();
 } 
@@ -19,15 +20,26 @@ initUser = async () => {
 login = async () => {
     try {   
         await Moralis.Web3.authenticate();
-        console.log("ok!")
         initUser();
     } catch (error) {
-        console.log("ne ok!")
-
         alert(error)
     }
 }
 
+logout = async () => {
+    await Moralis.User.logOut();
+    hideElement(userInfo);
+    initUser();
+}
+
+openUserInfo = async () => {
+    user = await Moralis.User.current();
+    if (user){    
+        showElement(userInfo);
+    }else{
+        login();
+    }
+}
 
 hideElement = (element) => element.style.display = "none";
 showElement = (element) => element.style.display = "block";
@@ -37,6 +49,14 @@ const userConnectButton = document.getElementById("btnConnect");
 userConnectButton.onclick = login;
 
 const userProfileButton = document.getElementById("btnUserInfo");
+userProfileButton.onclick = openUserInfo;
+
 // userProfileButton.onclick = openUserInfo;
+
+const userinfo  = document.getElementById("userinfo");
+document.getElementById("btnCloseUserInfo").onclick = () => hideElement(userInfo);
+document.getElementById("btnLogout").onclick = logout;
+
+
 
 init();
